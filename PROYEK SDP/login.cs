f7236a020c0ebc9a50ed5d286f1b3b4d73a23ec0
Max <maximillianhermawan@gmail.com>
2019-10-03 16:48:22 +0700

    CobaGit

--- PROYEK SDP/login.cs
+++ PROYEK SDP/login.cs
@@ -14,15 +14,48 @@ namespace PROYEK_SDP
     
     public partial class login : Form
     {
+        OracleConnection conn;
         public login()
         {
             InitializeComponent(); Master m = new Master();
             m.Visible = true;
+            try
+            {
+                conn = new OracleConnection("user id=system;password=maximillian99");
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void login_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = textBox1.Text;
+            string pass = textBox2.Text;
+
+            try
+            {
+                string query = "select * from pegawai where id_pegawai = '" + user + "'";
+                OracleDataAdapter oda = new OracleDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                oda.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    MessageBox.Show("Selamat Datang " + dt.Rows[0][1].ToString());
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
