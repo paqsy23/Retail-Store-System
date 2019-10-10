89343dbbc321a227d9792a8c97b06692d28fe4ee
Paqsy Jalasukma <aci.jalasukma@gmail.com>
2019-10-10 07:23:47 +0000

    Replace database.sql

--- database.sql
+++ database.sql
@@ -1,5 +1,4 @@
 drop table pegawai cascade constraint purge;
-drop table mobil cascade constraint purge;
 drop table barang cascade constraint purge;
 drop table buyer cascade constraint purge;
 drop table supplier cascade constraint purge;
@@ -18,12 +17,6 @@ create table pegawai (
 	nomor_telp varchar2(13)
 );
 
-create table mobil (
-	id_mobil varchar2(7) primary key, --- plat nomor
-	nama_mobil varchar2(15),
-	kapasitas number -- 12 pakaian = 1 box
-);
-
 create table jenis_barang (
 	id_jenis_barang varchar2(6) primary key, --- substr(nama_jenis_barang,1,2) + autogenerate
 	nama_jenis_barang varchar2(20)
@@ -35,13 +28,15 @@ create table gudang (
 );
 
 create table barang (
-	id_barang varchar2(7) primary key, --- BG + autogenerate + size(S,M,L,XL)
+	id_barang varchar2(8) primary key,
 	id_jenis_barang varchar2(6) constraint fk_idJenis references jenis_barang(id_jenis_barang),
 	id_gudang varchar2(6) constraint fk_idGud references gudang(id_gudang),
 	nama_barang varchar2(25),
+	warna_barang varchar2(15),
+	ukuran varchar2(3),
 	stock number,
 	harga_jual number,
-	harga_beli number
+	harga_beli number,
 );
 
 create table buyer (
@@ -69,13 +64,11 @@ create table htrans_in (
 
 create table dtrans_in (
 	id_htrans_in varchar2(12) constraint fk_Hin references htrans_in(id_htrans_in),
-	id_barang varchar2(7) constraint fk_brgHin references barang(id_barang),
+	id_barang varchar2(8) constraint fk_brgHin references barang(id_barang),
 	stock_masuk number,
 	harga_beli number,
 	subtotal number,
-	id_pegawai varchar(6) constraint fk_pegHin references pegawai(id_pegawai), --- pengurus
-	id_supir varchar2(6) constraint fk_sopHin references pegawai(id_pegawai), --- supir
-	id_mobil varchar2(7) constraint fk_mobHin references mobil(id_mobil)
+	id_penanggungjawab varchar(6) constraint fk_pegHin references pegawai(id_pegawai) --- pengurus
 );
 
 create table htrans_out (
@@ -87,14 +80,12 @@ create table htrans_out (
 
 create table dtrans_out (
 	id_htrans_out varchar2(12) constraint fk_Hout references htrans_out(id_htrans_out),
-	id_barang varchar2(7) constraint fk_brgHout references barang(id_barang),
+	id_barang varchar2(8) constraint fk_brgHout references barang(id_barang),
 	stock_keluar number,
 	harga_jual number,
 	subtotal number,
 	status number, --- 0: default, 1: cancel
-	id_pegawai varchar2(6) constraint fk_pegHout references pegawai(id_pegawai), --- pengurus
-	id_supir varchar2(6) constraint fk_sopHout references pegawai(id_pegawai), --- supir
-	id_mobil varchar2(7) constraint fk_mobHout references mobil(id_mobil)
+	id_penanggungjawab varchar2(6) constraint fk_pegHout references pegawai(id_pegawai) --- pengurus
 );
 
 create table temp_hpp ( --- untuk history update harga
@@ -105,6 +96,17 @@ create table temp_hpp ( --- untuk history update harga
 	harga_baru number --- hasil perhitungan
 );
 
+create table hPengiriman (
+	id_hPengiriman varchar2(12) primary key,
+	tanggal_pengiriman date,
+	id_supir varchar2(6) constraint fk_pegKirim references pegawai(id_pegawai)
+);
+
+create table dPengiriman (
+	id_hPengiriman varchar2(12) constraint fk_hKirim references hPengiriman(id_hPengiriman),
+	id_htrans_out varchar2(12) constraint fk_hKirim_out references htrans_out(id_htrans_out)
+);
+
 insert into pegawai values('MAN001','Lee Philpott','Manager','Ngagel Jaya 54','MAN001','03160600606');
 insert into pegawai values('PEG001','Jonathan Dean','Pegawai','Darmokali V/10','PEG001','081323242089');
 insert into pegawai values('PEG002','Chris Greenhill','Pegawai','Ketintang II/35','PEG002','08793287610');
@@ -112,11 +114,6 @@ insert into pegawai values('PEG003','Jonny Hughes','Pegawai','Jetis Wetan 97','P
 insert into pegawai values('SUP001','Graham Duffield','Supir','Dukuh Pakis X/76','SUP001','0317325638');
 insert into pegawai values('SUP002','Magno Vieira','Supir','Raya Darmo 59','SUP001','088880829101');
 insert into pegawai values('ADMIN','Admin','Admin','-','admin','-');
-
-insert into mobil values('L8906XE','Hi Max',20);
-insert into mobil values('W7549YX','L300',15);
-insert into mobil values('S1236OP','Carry Pickup',10);
-insert into mobil values('L8932IL','Grand Max',18);
 
 insert into jenis_barang values('KE0001','Kemeja');
 insert into jenis_barang values('KA0001','Kaos');
@@ -128,12 +125,12 @@ insert into gudang values('GD0001','Basuki Rahmat 98');
 insert into gudang values('GD0002','Keputran 123');
 insert into gudang values('GD0003','A Yani 67');
 
-insert into barang values('BG0001S','KE0001','GD0002','Kemeja Kotak Merah (S)',10,70000,85000);
-insert into barang values('BG0001M','KA0001','GD0001','Kaos Merah (M)',15,60000,75000);
-insert into barang values('BG001XL','KP0001','GD0002','Kemeja Panjang Biru (XL)',20,85000,95000);
-insert into barang values('BG0002S','CP0002','GD0002','Celana Pendek Jeans (S)',15,75000,90000);
-insert into barang values('BG0001L','KE0001','GD0003','Kemeja Kotak Hijau (L)',15,70000,85000);
-insert into barang values('BG0001M','CP0001','GD0002','Celana Panjang Jeans (M)',10,80000,95000);
+insert into barang values('KKRE1001','KE0001','GD0002','Kemeja Kotak','Red','S',10,70000,85000);
+insert into barang values('KAWH2001','KA0001','GD0001','Kaos','White','M',15,60000,75000);
+insert into barang values('KPGR4001','KP0001','GD0002','Kemeja Panjang','Hijau','XL',20,85000,95000);
+insert into barang values('CPBL1001','CP0002','GD0002','Celana Pendek Jeans','Biru','S',15,75000,90000);
+insert into barang values('KKYE3001','KE0001','GD0003','Kemeja Kotak','Kuning','L',15,70000,85000);
+insert into barang values('CPBL2001','CP0001','GD0002','Celana Panjang Jeans','Hitam','M',10,80000,95000);
 
 insert into buyer values('BY0001','David Bosumtwe','Ngagel Tengah IV/9','david1@gmail.com',1);
 insert into buyer values('BY0002','Adam Bouskouchi','Kupang Krajan V/2','adam1@gmail.com',0);
