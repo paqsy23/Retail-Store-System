88982391bbfb02f739900bbb3487b55e64c8f809
Paqsy Jalasukma <paqsy1@mhs.stts.edu>
2019-12-09 09:07:07 +0700

    mau pull

--- PROYEK SDP/SuratJalan1.cs
+++ PROYEK SDP/SuratJalan1.cs
@@ -87,6 +87,14 @@ namespace PROYEK_SDP {
                 return this.ReportDefinition.Sections[4];
             }
         }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_idHpengiriman {
+            get {
+                return this.DataDefinition.ParameterFields[0];
+            }
+        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
